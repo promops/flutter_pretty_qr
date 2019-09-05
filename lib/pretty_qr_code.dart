ea49243b10c00dd487aa3f5b80bce857e48cc56e
kompotiq <flip.oleg@gmail.com>
2019-09-05 16:12:39 +0300

    Round inner edges

--- lib/pretty_qr_code.dart
+++ lib/pretty_qr_code.dart
@@ -6,22 +6,26 @@ import 'package:qr/qr.dart';
 class PrettyQr extends StatelessWidget {
   ///Widget size
   final double size;
+
   ///Qr code data
   final String data;
+
   ///Square color
   final Color elementColor;
+
   ///Error correct level
   final int errorCorrectLevel;
+
   ///Round the corners
   bool roundEdges;
 
   PrettyQr(
       {Key key,
-        this.size = 100,
-        @required this.data,
-        this.elementColor = Colors.black,
-        this.errorCorrectLevel = QrErrorCorrectLevel.M,
-        this.roundEdges = false})
+      this.size = 100,
+      @required this.data,
+      this.elementColor = Colors.black,
+      this.errorCorrectLevel = QrErrorCorrectLevel.M,
+      this.roundEdges = false})
       : super(key: key);
 
   @override
@@ -47,8 +51,11 @@ class PrettyQrCodePainter extends CustomPainter {
   QrCode _qrCode;
 
   PrettyQrCodePainter(
-      {this.data, this.elementColor = Colors.black, this.errorCorrectLevel = QrErrorCorrectLevel.M, this.roundEdges = false}) {
-    _qrCode = QrCode(4, errorCorrectLevel);
+      {this.data,
+      this.elementColor = Colors.black,
+      this.errorCorrectLevel = QrErrorCorrectLevel.M,
+      this.roundEdges = false}) {
+    _qrCode = QrCode(1, errorCorrectLevel);
     _qrCode.addData(data);
     _qrCode.make();
   }
@@ -62,6 +69,11 @@ class PrettyQrCodePainter extends CustomPainter {
     var _paint = Paint()
       ..style = PaintingStyle.fill
       ..color = this.elementColor
+      ..isAntiAlias = true;
+
+    var _paintBackground = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.white
       ..isAntiAlias = true;
 
     List<List> matrix = List<List>(_qrCode.moduleCount + 2);
@@ -91,12 +103,73 @@ class PrettyQrCodePainter extends CustomPainter {
       for (int y = 0; y < _qrCode.moduleCount; y++) {
         if (_qrCode.isDark(y, x)) {
           final Rect squareRect =
-          Rect.fromLTWH(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+              Rect.fromLTWH(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 
           _setShape(x + 1, y + 1, squareRect, _paint, matrix, canvas,
               _qrCode.moduleCount);
+        } else {
+          canvas.drawRect(
+              Rect.fromLTWH(x * pixelSize, y * pixelSize, pixelSize, pixelSize),
+              _paint);
+
+          _setShapeInner(
+              x + 1,
+              y + 1,
+              Rect.fromLTWH(x * pixelSize, y * pixelSize, pixelSize, pixelSize),
+              _paintBackground,
+              matrix,
+              canvas,
+              size.width);
         }
       }
+    }
+  }
+
+  //Скругляем внутренние углы (фоновым цветом)
+  void _setShapeInner(
+      int x, int y, Rect squareRect, Paint paint, List matrix, Canvas canvas, double pixelSize) {
+    bool bottomRight = false;
+    bool bottomLeft = false;
+    bool topRight = false;
+    bool topLeft = false;
+
+    
+
+    //bottom right check
+    if (matrix[y + 1][x] && matrix[y][x + 1] && matrix[y + 1][x + 1]) {
+      bottomRight = true;
+    }
+
+    //top left check
+    if (matrix[y - 1][x] && matrix[y][x - 1] && matrix[y - 1][x - 1]) {
+      topLeft = true;
+    }
+
+    //bottom left check
+    if (matrix[y + 1][x] && matrix[y][x - 1] && matrix[y + 1][x - 1]) {
+      bottomLeft = true;
+    }
+
+    //top right check
+    if (matrix[y - 1][x] && matrix[y][x + 1] && matrix[y - 1][x + 1]) {
+      topRight = true;
+    }
+
+    canvas.drawRRect(
+        RRect.fromRectAndCorners(
+          squareRect,
+          bottomRight: bottomRight ? Radius.circular(3.0) : Radius.zero,
+          bottomLeft: bottomLeft ? Radius.circular(3.0) : Radius.zero,
+          topLeft: topLeft ? Radius.circular(3.0) : Radius.zero,
+          topRight: topRight ? Radius.circular(3.0) : Radius.zero,
+        ),
+        paint);
+
+    //if it is dot (arount an empty place)
+    if (!bottomLeft && !bottomRight && !topLeft && !topRight) {
+      canvas.drawRect(
+          Rect.fromLTWH(x * pixelSize, y * pixelSize, pixelSize, pixelSize),
+          paint);
     }
   }
 
@@ -113,8 +186,15 @@ class PrettyQrCodePainter extends CustomPainter {
         !matrix[y][x + 1] &&
         !matrix[y - 1][x] &&
         !matrix[y][x - 1]) {
-      canvas.drawRRect(
-          RRect.fromRectAndRadius(squareRect, Radius.circular(4.0)), paint);
+     canvas.drawRRect(
+        RRect.fromRectAndCorners(
+          squareRect,
+          bottomRight:  Radius.circular(2.5),
+          bottomLeft:  Radius.circular(2.5),
+          topLeft:  Radius.circular(2.5),
+          topRight:  Radius.circular(2.5) 
+        ),
+        paint);
       return;
     }
 
@@ -141,18 +221,15 @@ class PrettyQrCodePainter extends CustomPainter {
     canvas.drawRRect(
         RRect.fromRectAndCorners(
           squareRect,
-          bottomRight: bottomRight ? Radius.circular(8.0) : Radius.zero,
-          bottomLeft: bottomLeft ? Radius.circular(8.0) : Radius.zero,
-          topLeft: topLeft ? Radius.circular(8.0) : Radius.zero,
-          topRight: topRight ? Radius.circular(8.0) : Radius.zero,
+          bottomRight: bottomRight ? Radius.circular(6.0) : Radius.zero,
+          bottomLeft: bottomLeft ? Radius.circular(6.0) : Radius.zero,
+          topLeft: topLeft ? Radius.circular(6.0) : Radius.zero,
+          topRight: topRight ? Radius.circular(6.0) : Radius.zero,
         ),
         paint);
 
     //if it is dot (arount an empty place)
-    if (!bottomLeft &&
-        !bottomRight &&
-        !topLeft &&
-        !topRight) {
+    if (!bottomLeft && !bottomRight && !topLeft && !topRight) {
       canvas.drawRect(squareRect, paint);
     }
   }
