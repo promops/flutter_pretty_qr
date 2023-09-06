fe6041fd945d398553a26d7b59d2d8374a6d0d68
Egor Popov <nxtswitch@gmail.com>
2023-09-06 15:47:39 +0300

    refactor: simplify toImage method

--- lib/src/painting/extensions/pretty_qr_image_extensions.dart
+++ lib/src/painting/extensions/pretty_qr_image_extensions.dart
@@ -6,6 +6,7 @@ import 'package:qr/qr.dart';
 import 'package:flutter/rendering.dart';
 
 import 'package:pretty_qr_code/src/base/pretty_qr_matrix.dart';
+import 'package:pretty_qr_code/src/painting/pretty_qr_painter.dart';
 import 'package:pretty_qr_code/src/painting/pretty_qr_decoration.dart';
 import 'package:pretty_qr_code/src/rendering/pretty_qr_painting_context.dart';
 
@@ -16,10 +17,10 @@ extension PrettyQrImageExtension on QrImage {
     required final double size,
     final PrettyQrDecoration decoration = const PrettyQrDecoration(),
     final ImageConfiguration configuration = ImageConfiguration.empty,
-  }) async {
+  }) {
     final offsetLayer = OffsetLayer();
     final imageCompleter = Completer<ui.Image>();
-    final decorationPainter = decoration.createPainter(() {});
+    final imageConfiguration = configuration.copyWith(size: Size.square(size));
 
     final context = PrettyQrPaintingContext(
       offsetLayer,
@@ -28,57 +29,53 @@ extension PrettyQrImageExtension on QrImage {
       textDirection: configuration.textDirection,
     );
 
-    Future<ui.Image> captureQRImage() async {
-      decorationPainter.paint(
-        context,
-        configuration.copyWith(size: Size.square(size)),
-      );
-
+    void captureQRImage() {
       // ignore: invalid_use_of_protected_member
       context.stopRecordingIfNeeded();
-      await Future.delayed(Duration.zero);
-
-      final image = offsetLayer.toImageSync(
-        context.estimatedBounds,
-        pixelRatio: configuration.devicePixelRatio ?? 1.0,
+      imageCompleter.complete(
+        offsetLayer.toImageSync(
+          context.estimatedBounds,
+          pixelRatio: configuration.devicePixelRatio ?? 1.0,
+        ),
       );
-
-      return image;
     }
 
-    final decorationImageStreamListener = ImageStreamListener(
-      (imageInfo, synchronous) async {
-        try {
-          final image = await captureQRImage();
-          imageCompleter.complete(image);
-        } finally {
-          imageInfo.dispose();
-        }
-      },
-      onError: (error, stack) async {
-        throw Error.throwWithStackTrace(error, stack ?? StackTrace.current);
-      },
-    );
+    late final PrettyQrPainter decorationPainter;
+    try {
+      decorationPainter = decoration.createPainter(() {
+        decorationPainter.paint(context, imageConfiguration);
+        captureQRImage();
+      });
+      decorationPainter.paint(context, imageConfiguration);
 
-    final decorationImageStream = runZonedGuarded(
-      () {
-        return decoration.image?.image.resolve(
-          configuration,
-        )?..addListener(decorationImageStreamListener);
-      },
-      (error, stackTrace) {
-        imageCompleter.completeError(error, stackTrace);
-      },
-    );
+      final decorationImageStream = decoration.image?.image.resolve(
+        configuration,
+      );
 
-    if (!imageCompleter.isCompleted && decorationImageStream == null) {
-      imageCompleter.complete(captureQRImage());
+      if (decorationImageStream == null) {
+        captureQRImage();
+      } else {
+        late final ImageStreamListener imageStreamListener;
+        imageStreamListener = ImageStreamListener(
+          (imageInfo, synchronous) {
+            decorationImageStream.removeListener(imageStreamListener);
+            imageInfo.dispose();
+            if (synchronous) captureQRImage();
+          },
+          onError: (error, stackTrace) {
+            decorationImageStream.removeListener(imageStreamListener);
+            imageCompleter.completeError(error, stackTrace);
+          },
+        );
+        decorationImageStream.addListener(imageStreamListener);
+      }
+    } catch (error, stackTrace) {
+      imageCompleter.completeError(error, stackTrace);
     }
 
     return imageCompleter.future.whenComplete(() {
       offsetLayer.dispose();
       decorationPainter.dispose();
-      decorationImageStream?.removeListener(decorationImageStreamListener);
     });
   }
 
