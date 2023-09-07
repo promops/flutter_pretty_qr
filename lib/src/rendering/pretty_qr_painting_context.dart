5664068a36b931453e561b12b6e0c507667d6d4a
Egor Popov <nxtswitch@gmail.com>
2023-09-07 11:50:18 +0300

    refactor: separate qr code painter

--- lib/src/rendering/pretty_qr_painting_context.dart
+++ lib/src/rendering/pretty_qr_painting_context.dart
@@ -1,5 +1,5 @@
 import 'package:meta/meta.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:pretty_qr_code/src/base/pretty_qr_matrix.dart';
 
@@ -8,13 +8,13 @@ import 'package:pretty_qr_code/src/base/pretty_qr_matrix.dart';
 /// {@endtemplate}
 @sealed
 class PrettyQrPaintingContext {
-  /// The bounds within which the painting context's [canvas] will record
-  /// painting commands.
-  final Rect bounds;
-
   /// The canvas on which to paint.
   @nonVirtual
   final Canvas canvas;
+
+  /// The bounds within which the painting context's [canvas] will record
+  /// painting commands.
+  final Rect estimatedBounds;
 
   /// {@macro pretty_qr_code.PrettyQrMatrix}
   @nonVirtual
@@ -26,9 +26,9 @@ class PrettyQrPaintingContext {
 
   /// Creates a QR painting context.
   @literal
-  const PrettyQrPaintingContext({
-    required this.bounds,
-    required this.canvas,
+  const PrettyQrPaintingContext(
+    this.canvas,
+    this.estimatedBounds, {
     required this.matrix,
     this.textDirection,
   });
