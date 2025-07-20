5c3e5fbeaf807b1539638b8ebd740053410d056b
Egor Popov <nxtswitch@gmail.com>
2025-07-20 22:58:07 +0300

    feat: add experimental custom shape

--- lib/src/painting/pretty_qr_shape.dart
+++ lib/src/painting/pretty_qr_shape.dart
@@ -1,5 +1,8 @@
 import 'package:meta/meta.dart';
 
+import 'package:pretty_qr_code/src/base/pretty_qr_matrix.dart';
+import 'package:pretty_qr_code/src/base/components/pretty_qr_component.dart';
+
 import 'package:pretty_qr_code/src/rendering/pretty_qr_painting_context.dart';
 
 /// {@template pretty_qr_code.painting.PrettyQrShape}
@@ -11,6 +14,22 @@ abstract class PrettyQrShape {
   /// const constructors so that they can be used in const expressions.
   @literal
   const PrettyQrShape();
+
+  /// Creates a custom shape.
+  ///
+  /// The `finderPattern` shape applies to three Position Detection Patterns,
+  /// the `timingPatterns` shape applies to horizontal and vertical Timing
+  /// Patterns, and the`alignmentPatterns` shape applies to Alignment Patterns.
+  ///
+  /// All the shapes default to [shape] argument.
+  @literal
+  @experimental
+  const factory PrettyQrShape.custom(
+    final PrettyQrShape shape, {
+    final PrettyQrShape? finderPattern,
+    final PrettyQrShape? timingPatterns,
+    final PrettyQrShape? alignmentPatterns,
+  }) = PrettyQrCustomShape;
 
   /// Linearly interpolates from another [PrettyQrShape] (which may be of a
   /// different class) to `this`.
@@ -49,5 +68,160 @@ abstract class PrettyQrShape {
     if (t == 1.0) return b;
 
     return b.lerpFrom(a, t) ?? a.lerpTo(b, t) ?? b;
+  }
+}
+
+/// A class for creates custom QR Code shape.
+@sealed
+@experimental
+class PrettyQrCustomShape extends PrettyQrShape {
+  @nonVirtual
+  final PrettyQrShape shape;
+
+  @nonVirtual
+  final PrettyQrShape? finderPattern;
+
+  @nonVirtual
+  final PrettyQrShape? timingPatterns;
+
+  @nonVirtual
+  final PrettyQrShape? alignmentPatterns;
+
+  /// Creates a custom QR Code shape.
+  @literal
+  const PrettyQrCustomShape(
+    this.shape, {
+    this.finderPattern,
+    this.timingPatterns,
+    this.alignmentPatterns,
+  });
+
+  @override
+  void paint(PrettyQrPaintingContext context) {
+    final hasFinderPattern = finderPattern != null;
+    final hasTimingPatterns = timingPatterns != null;
+    final hasAlignmentPatterns = alignmentPatterns != null;
+
+    if (!hasFinderPattern && !hasTimingPatterns && !hasAlignmentPatterns) {
+      shape.paint(context);
+      return;
+    }
+
+    final matrix = PrettyQrMatrix.masked(
+      context.matrix,
+      exclude: {
+        if (hasFinderPattern) PrettyQrComponentType.finderPattern,
+        if (hasTimingPatterns) PrettyQrComponentType.timingPattern,
+        if (hasAlignmentPatterns) PrettyQrComponentType.alignmentPattern,
+      },
+    );
+    shape.paint(context.copyWith(matrix: matrix));
+
+    if (hasFinderPattern) {
+      finderPattern?.paint(
+        context.copyWith(
+          matrix: PrettyQrMatrix.masked(
+            context.matrix,
+            exclude: {
+              for (final type in PrettyQrComponentType.values)
+                if (type != PrettyQrComponentType.finderPattern) type,
+            },
+          ),
+        ),
+      );
+    }
+
+    if (hasTimingPatterns) {
+      timingPatterns?.paint(
+        context.copyWith(
+          matrix: PrettyQrMatrix.masked(
+            context.matrix,
+            exclude: {
+              for (final type in PrettyQrComponentType.values)
+                if (type != PrettyQrComponentType.timingPattern) type,
+            },
+          ),
+        ),
+      );
+    }
+
+    if (hasAlignmentPatterns) {
+      alignmentPatterns?.paint(
+        context.copyWith(
+          matrix: PrettyQrMatrix.masked(
+            context.matrix,
+            exclude: {
+              for (final type in PrettyQrComponentType.values)
+                if (type != PrettyQrComponentType.alignmentPattern) type,
+            },
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  PrettyQrCustomShape? lerpFrom(PrettyQrShape? a, double t) {
+    if (identical(a, this)) {
+      return this;
+    }
+
+    if (a == null) return this;
+    if (a is! PrettyQrCustomShape) return null;
+
+    if (t == 0.0) return a;
+    if (t == 1.0) return this;
+
+    const lerp = PrettyQrShape.lerp;
+    return PrettyQrCustomShape(
+      lerp(a.shape, shape, t)!,
+      finderPattern: lerp(a.finderPattern, finderPattern, t)!,
+      timingPatterns: lerp(a.timingPatterns, timingPatterns, t)!,
+      alignmentPatterns: lerp(a.alignmentPatterns, alignmentPatterns, t)!,
+    );
+  }
+
+  @override
+  PrettyQrCustomShape? lerpTo(PrettyQrShape? b, double t) {
+    if (identical(this, b)) {
+      return this;
+    }
+
+    if (b == null) return this;
+    if (b is! PrettyQrCustomShape) return null;
+
+    if (t == 0.0) return this;
+    if (t == 1.0) return b;
+
+    const lerp = PrettyQrShape.lerp;
+    return PrettyQrCustomShape(
+      lerp(shape, b.shape, t)!,
+      finderPattern: lerp(finderPattern, b.finderPattern, t)!,
+      timingPatterns: lerp(timingPatterns, b.timingPatterns, t)!,
+      alignmentPatterns: lerp(alignmentPatterns, b.alignmentPatterns, t)!,
+    );
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      shape,
+      finderPattern,
+      timingPatterns,
+      alignmentPatterns,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) {
+      return true;
+    }
+
+    return other is PrettyQrCustomShape &&
+        other.shape == shape &&
+        other.finderPattern == finderPattern &&
+        other.timingPatterns == timingPatterns &&
+        other.alignmentPatterns == alignmentPatterns;
   }
 }
